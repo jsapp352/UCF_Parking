a1d7eea3c952252d73a6bcc716851295964b211d
Justin Sapp <jsapp@knights.ucf.edu>
2019-01-15 23:03:57 -0500

    Tested Google Maps API -- Geocoding request successful

--- parking.py
+++ parking.py
@@ -1,11 +1,16 @@
+import googlemaps
 
 def main():
     # Load Google Maps API key from text file (file not included in Git repository)
     with open('googlemap_key.txt', 'r') as f:
         api_key = f.read()
 
-    #DEBUG
-    print(api_key)
+    gmaps = googlemaps.Client(key = api_key)
+
+    geocode_result = gmaps.geocode('4000 Central Florida Blvd, Orlando, FL 32816')
+
+    print(geocode_result)
+
 
 if __name__ == '__main__':
     main()
