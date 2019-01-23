d76ad9d925ab215ce70c9b8c8b0d259886f99c7c
Justin Sapp <jsapp@knights.ucf.edu>
2019-01-22 20:19:49 -0500

    Added comments, moved some code sections from main() to more appropriate, already existing functions

--- parking.py
+++ parking.py
@@ -2,11 +2,12 @@ import datetime
 import googlemaps
 import urllib2
 
-def get_distance_to_garage(api_key, origin, destination, garage_keys):
+def get_travel_times(api_key, origin, destination, garage_data):
     gmaps = googlemaps.Client(key = api_key)
-
+    garage_keys = garage_data.keys()
     garages = ['UCF Parking Garage {0}'.format(name) for name in garage_keys]
 
+    # There may be a better way to extract travel durations from the distance matrices
     driving_distances = gmaps.distance_matrix(origin, garages, mode='driving')['rows'][0]['elements']
     driving_durations = [x['duration']['value'] for x in driving_distances]
 
@@ -15,15 +16,24 @@ def get_distance_to_garage(api_key, origin, destination, garage_keys):
 
     total_durations = [sum(x) for x in zip(driving_durations, walking_durations)]
 
-    return zip(garage_keys, driving_durations, walking_durations, total_durations)
+    garage_distances = zip(garage_keys, driving_durations, walking_durations, total_durations)
 
-def get_garage_permission(garage_name):
+    # Add travel durations to garage_data dictionary
+    for garage in garage_distances:
+        garage_data[garage[0]]['driving_duration'] = garage[1]
+        garage_data[garage[0]]['walking_duration'] = garage[2]
+        garage_data[garage[0]]['travel_duration'] = garage[1] + garage[2]
+
+    # Sort garages by total travel duration
+    garage_distances.sort(key=lambda tup: tup[3])
+
+    # Return list of garage dictionary keys sorted by travel time
+    return [garage[0] for garage in garage_distances]
+
+def has_garage_permission(garage_name):
     permit_d = ['A', 'B', 'C', 'D', 'G', 'H', 'I']
 
-    if garage_name in permit_d:
-        return 'D'
-    else:
-        return None
+    return garage_name in permit_d
 
 def get_garage_data(garage_url):
     # Create an empty dictionary to hold garage data
@@ -44,8 +54,9 @@ def get_garage_data(garage_url):
         total_spots = int(search[0].rstrip())
         search = search[2].partition('DataRow_DevEx')[2].partition('Garage ')[2].partition('</td>')
 
-        # If the garage has open spots, add its data (as nested dict.) to the main garage dictionary
-        if get_garage_permission(garage_name) == 'D' and available_spots > 0:
+        # If the garage has open spots and the correct parking permit type,
+        # add its data (as nested dict.) to the main garage dictionary
+        if has_garage_permission(garage_name) and available_spots > 0:
             garage = {
             'available': available_spots,
             'capacity': total_spots
@@ -59,39 +70,37 @@ def main():
     api_key_filename = 'googlemap_key.txt'
     garage_data_url = 'http://secure.parking.ucf.edu/GarageCount/'
 
+    origin = '1815 S Semoran Blvd, Orlando, FL 32822'
+    destination = 'Harris Corporation Engineering Center, Orlando, FL, 32816'
+
+    # Print the intended origin and destination locations
+    print('Origin: {0}'.format(origin))
+    print('Destination: {0}\n'.format(destination))
+
     # Load Google Maps API key from text file (file not included in Git repository)
     with open(api_key_filename, 'r') as f:
         api_key = f.read()
 
+    # Retrieve garage availability data from UCF Parking Services
     garage_data = get_garage_data(garage_data_url)
 
-    origin = '1815 S Semoran Blvd, Orlando, FL 32822'
-    destination = 'Harris Corporation Engineering Center, Orlando, FL, 32816'
+    # Use Google Maps API to find driving/walking times for each garage
+    sorted_garage_keys = get_travel_times(api_key, origin, destination, garage_data)
 
-    print('Origin: {0}'.format(origin))
-    print('Destination: {0}\n'.format(destination))
-
-    garage_distances = get_distance_to_garage(api_key, origin, destination, garage_data.keys())
-
-    # Add travel durations to garage_data dictionary
-    for garage in garage_distances:
-        garage_data[garage[0]]['driving_duration'] = garage[1]
-        garage_data[garage[0]]['walking_duration'] = garage[2]
-        garage_data[garage[0]]['travel_duration'] = garage[1] + garage[2]
-
-    garage_distances.sort(key=lambda tup: tup[3])
-
-    garage = garage_distances[0]
-
+    # Print final results
     print('Garages listed by total travel time:\n')
+    for garage in sorted_garage_keys:
+        g = garage_data[garage]
 
-    for garage in garage_distances:
-        print('  Garage {0}\n'.format(garage[0]))
-        print('  Travel time: {0}\n'.format(str(datetime.timedelta(seconds=garage[3]))))
-        print('    Driving time: {0}'.format(str(datetime.timedelta(seconds=garage[1]))))
-        print('    Walking time: {0}\n'.format(str(datetime.timedelta(seconds=garage[2]))))
-        print('  Available spaces: {0} of {1}\n\n'.format(garage_data[garage[0]]['available'],
-        garage_data[garage[0]]['capacity']))
+        travel_time = str(datetime.timedelta(seconds = g['travel_duration']))
+        driving_time = str(datetime.timedelta(seconds = g['driving_duration']))
+        walking_time = str(datetime.timedelta(seconds = g['walking_duration']))
+
+        print('  Garage {0}\n'.format(garage))
+        print('  Travel time: {0}\n'.format(travel_time))
+        print('    Driving time: {0}'.format(driving_time))
+        print('    Walking time: {0}\n'.format(walking_time))
+        print('  Available spaces: {0} of {1}\n\n'.format(g['available'], g['capacity']))
 
 if __name__ == '__main__':
     main()
